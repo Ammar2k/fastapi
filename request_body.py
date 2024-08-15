8541f714c8fcc6829e4952b6c86ce03272c1f624
Ammar <muhammad2000ammar@gmail.com>
2024-08-15 22:30:56 +0200

    request body + path param + query param

--- request_body.py
+++ request_body.py
@@ -17,3 +17,10 @@ async def create_item(item: Item):
         price_with_tax = item.price + item.tax
         item_dict.update({"price_with_tax": price_with_tax})
     return item
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump}
+    if q:
+        result.update({"q": q})
+    return result
