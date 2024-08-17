e835c2cd247997706342a615710970ec90677a7e
Ammar <muhammad2000ammar@gmail.com>
2024-08-17 12:08:51 +0200

    metadata to query

--- string_validation.py
+++ string_validation.py
@@ -5,8 +5,17 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Annotated[str | None, Query(min_length=3, max_length=50)] = None):
-    results = {"items": [{"items": "Foo"}, {"item_id": "Bar"}]}
+async def read_items(
+    q: Annotated[
+        str | None, 
+        Query(
+            title="Query String", 
+            description="Qeury String for the items to search in the database that have a good match",
+            min_length=3,
+        ),
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
     return results
