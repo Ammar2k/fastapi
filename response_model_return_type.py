a39d24ee466ac07f21293fae14812fd810dedefc
Ammar <muhammad2000ammar@gmail.com>
2024-08-30 16:11:26 +0200

    username and password pydantic model

--- response_model_return_type.py
+++ response_model_return_type.py
@@ -1,28 +1,16 @@
-from typing import Any
-
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name:str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    tags: list[str] = []
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: str | None = None
 
 
-
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item) -> Any:
-    return item
-
-
-@app.get("/items/", response_model=list[Item])
-async def read_items() -> Any:
-    return [
-        {"name":"Portal Gun", "price": 42.0},
-        {"name": "Plumbus", "price": 32.0},
-    ]
+@app.post("/user/")
+async def create_user(user: UserIn) -> UserIn:
+    return user
